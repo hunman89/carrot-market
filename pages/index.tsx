e50c27ad45a92ee78a4f4276d3d9ef9de5e68b27
hunman89 <hunman89@gmail.com>
2022-02-03 14:22:50 +0900

    add. tailwind css dark mode modifier

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,25 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className=" bg-slate-400 py-20 px-20 grid xl:place-content-center gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
-      <div className="bg-white flex flex-col justify-between p-6 rounded-3xl shadow-xl">
-        <span className=" font-semibold text-3xl">Select Item</span>
+      <div className="bg-white dark:bg-black flex flex-col justify-between p-6 rounded-3xl shadow-xl">
+        <span className="dark:text-white font-semibold text-3xl">
+          Select Item
+        </span>
         <ul>
           {[1, 2].map((i) => (
-            <div
-              key={i}
-              className="flex justify-between my-2 odd:bg-blue-50 even:bg-yellow-50"
-            >
-              <span className=" text-gray-500">Gray Chair</span>
-              <span className="font-semibold">$19</span>
+            <div key={i} className="flex justify-between my-2">
+              <span className=" text-gray-500 dark:text-gray-50">
+                Gray Chair
+              </span>
+              <span className="dark:text-gray-50 font-semibold">$19</span>
             </div>
           ))}
         </ul>
         <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
-          <span className="">Total</span>
-          <span className="font-semibold">$10</span>
+          <span className="dark:text-gray-50">Total</span>
+          <span className="dark:text-gray-50  font-semibold">$10</span>
         </div>
-        <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto hover:bg-teal-500 active:bg-yellow-500 focus:bg-red-500">
+        <button className="mt-5 bg-blue-500 dark:bg-black dark:border-2 dark:border-white text-white p-3 text-center rounded-xl w-1/2 mx-auto dark:hover:bg-white dark:hover:text-black hover:bg-teal-500 active:bg-yellow-500 focus:bg-red-500">
           Checkout
         </button>
       </div>
