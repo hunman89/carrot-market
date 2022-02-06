1c49b167624e72f0796255ccc2e4ebab5ec437cf
hunman89 <hunman89@gmail.com>
2022-02-06 11:11:42 +0900

    add. change stream detail and item upload page

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Upload: NextPage = () => {
   return (
-    <div className="px-4 py-16">
+    <div className="px-4 py-16 space-y-5">
       <div>
         <label className="w-full cursor-pointer transition-color text-gray-600 hover:text-orange-500 hover:border-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md">
           <svg
@@ -26,7 +26,22 @@ const Upload: NextPage = () => {
           />
         </label>
       </div>
-      <div className="my-5">
+      <div>
+        <label
+          className="mb-1 block text-sm font-medium text-gray-500"
+          htmlFor="name"
+        >
+          Name
+        </label>
+        <div className="rounded-md relative flex items-center shadow-sm">
+          <input
+            id="name"
+            type="text"
+            className="appearance-none py-2 w-full px-3 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+          />
+        </div>
+      </div>
+      <div>
         <label
           className="mb-1 block text-sm font-medium text-gray-500"
           htmlFor="price"
@@ -62,7 +77,7 @@ const Upload: NextPage = () => {
         />
       </div>
       <button className="mt-4 w-full bg-orange-500 hover:bg-orange-600 text-white px-3 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
-        Upload product
+        Upload item
       </button>
     </div>
   );
