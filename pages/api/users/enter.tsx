78a1a1cae57fef2bc8982f876fa74b64c46a6514
hunman89 <hunman89@gmail.com>
2022-02-17 18:57:59 +0900

    add. create token with connect or create user

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,51 +5,21 @@ import withHandler from "@libs/server/withHandler";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: { ...payload },
-    create: {
-      name: "Anonymous",
-      ...payload,
+  const token = await client.token.create({
+    data: {
+      payload: "12345",
+      user: {
+        connectOrCreate: {
+          where: { ...payload },
+          create: {
+            name: "Anonymous",
+            ...payload,
+          },
+        },
+      },
     },
-    update: {},
   });
-  console.log(user);
-  /* if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (user) console.log("found");
-    if (!user) {
-      console.log("create user");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          email,
-        },
-      });
-    }
-    console.log(user);
-  }
-  if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone,
-      },
-    });
-    if (user) console.log("found");
-    if (!user) {
-      console.log("create user");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          phone: +phone,
-        },
-      });
-    }
-    console.log(user);
-  } */
+  console.log(token);
   return res.status(200).end();
 }
 
