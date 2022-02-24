900a8a47342bcadde11b161f795044ffeb375338
hunman89 <hunman89@gmail.com>
2022-02-24 17:14:32 +0900

    add. favorite product

--- components/button.tsx
+++ components/button.tsx
@@ -17,7 +17,7 @@ export default function Button({
       {...rest}
       className={cls(
         large ? "py-3 text-base" : "py-2 text-sm ",
-        "mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white px-4 border border-transparent rounded-md shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none "
+        " w-full bg-orange-500 hover:bg-orange-600 text-white px-4 border border-transparent rounded-md shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none "
       )}
     >
       {text}
