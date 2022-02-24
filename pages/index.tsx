386ae116261b476b35ad054c1180cd39a0fb03e0
hunman89 <hunman89@gmail.com>
2022-02-24 13:21:34 +0900

    add. see products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,36 @@
 import useUser from "@libs/client/useUser";
+import { Product } from "@prisma/client";
 import type { NextPage } from "next";
 import Head from "next/head";
+import useSWR from "swr";
 import FloatingButton from "../components/floating-button";
 import Item from "../components/item";
 import Layout from "../components/layout";
 
+interface ProductResponse {
+  ok: boolean;
+  products: Product[];
+}
+
 const Home: NextPage = () => {
   const { user, isLoading } = useUser();
-  console.log(user);
+  const { data } = useSWR<ProductResponse>("/api/products");
   return (
     <Layout title="Home" hasTabBar>
       <Head>
         <title>Home</title>
       </Head>
       <div className="flex flex-col divide-y-2">
-        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map(
-          (_, i) => (
-            <Item
-              key={i}
-              id={i}
-              title="New iPhone 14"
-              price={95}
-              comments={1}
-              hearts={1}
-            />
-          )
-        )}
+        {data?.products?.map((product) => (
+          <Item
+            key={product.id}
+            id={product.id}
+            title={product.name}
+            price={product.price}
+            comments={1}
+            hearts={1}
+          />
+        ))}
         <FloatingButton href="/products/upload">
           <svg
             className="h-6 w-6"
