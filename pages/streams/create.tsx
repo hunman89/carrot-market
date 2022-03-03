9ada7130edc974905967bf646d7d4ab3bdc69f21
hunman89 <hunman89@gmail.com>
2022-03-03 21:29:54 +0900

    add. create stream page

--- pages/streams/create.tsx
+++ pages/streams/create.tsx
@@ -3,15 +3,63 @@ import Button from "@components/button";
 import Input from "@components/input";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutation";
+import { useEffect } from "react";
+import Id from "pages/api/products/[id]";
+import { useRouter } from "next/router";
+import { Stream } from "@prisma/client";
+
+interface CreateForm {
+  name: string;
+  price: string;
+  description: string;
+}
+
+interface CreateResponse {
+  ok: boolean;
+  stream: Stream;
+}
 
 const Create: NextPage = () => {
+  const router = useRouter();
+  const [createStream, { loading, data }] =
+    useMutation<CreateResponse>(`/api/streams`);
+  const { register, handleSubmit } = useForm<CreateForm>();
+  const onValid = (form: CreateForm) => {
+    if (loading) return;
+    createStream(form);
+  };
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/streams/${data.stream.id}`);
+    }
+  }, [data, router]);
   return (
     <Layout title="Do Stream" canGoBack>
-      <form className="px-4 space-y-5">
-        <Input label="Name" name="name" />
-        <Input label="Price" name="price" kind="price" placeholder="0.00" />
-        <TextArea label="Description" name="description" />
-        <Button text="Go live" />
+      <form onSubmit={handleSubmit(onValid)} className="px-4 space-y-5">
+        <Input
+          register={register("name", { required: true })}
+          label="Name"
+          type="text"
+          name="name"
+          required
+        />
+        <Input
+          register={register("price")}
+          required
+          type="text"
+          label="Price"
+          name="price"
+          kind="price"
+        />
+        <TextArea
+          register={register("description", { required: true })}
+          label="Description"
+          name="description"
+          required
+        />
+        <Button text={loading ? "Loading..." : "Go live"} />
       </form>
     </Layout>
   );
