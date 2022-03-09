d32309ba7c12e545e20ac8eab64a1641ed94cf07
hunman89 <hunman89@gmail.com>
2022-03-09 20:02:45 +0900

    add. data seed in prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import Item from "@components/item";
+import { PrismaClient } from "@prisma/client";
+import { disconnect } from "process";
+
+const client = new PrismaClient();
+
+async function main() {
+  [...Array.from(Array(100).keys())].forEach(async (item) => {
+    await client.stream.create({
+      data: {
+        name: String(item),
+        description: String(item),
+        price: item,
+        user: {
+          connect: {
+            id: 7,
+          },
+        },
+      },
+    });
+    setTimeout;
+    console.log(`${item}/100`);
+  });
+}
+
+main()
+  .catch((e) => console.log(e))
+  .finally(() => client.$disconnect());
