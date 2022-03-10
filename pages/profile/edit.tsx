0be87a4323a4ed6960bd253bc7f68426143610c1
hunman89 <hunman89@gmail.com>
2022-03-10 20:38:22 +0900

    add. upload image use cloudflare direct upload

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -37,7 +37,7 @@ const EditProfile: NextPage = () => {
   }, [user, setValue]);
   const [editProfile, { data, loading }] =
     useMutation<EditProfileResponse>(`/api/users/me`);
-  const onValid = ({ email, phone, name, avatar }: EditProfileForm) => {
+  const onValid = async ({ email, phone, name, avatar }: EditProfileForm) => {
     if (loading) return;
     if (email === "" && phone === "" && name === "") {
       return setError("formErrors", {
@@ -45,7 +45,18 @@ const EditProfile: NextPage = () => {
           "Email OR Phone number are required. you need to choose the one.,",
       });
     }
-    editProfile({ email, phone, name });
+    if (avatar && avatar.length > 0) {
+      const cfRequest = await (await fetch(`/api/files`)).json();
+      // upload file to url
+      editProfile({
+        email,
+        phone,
+        name,
+        //avatarUrl
+      });
+    } else {
+      editProfile({ email, phone, name });
+    }
   };
   useEffect(() => {
     if (data && !data.ok) {
