10acb4ed2513d50096c2f1431da32f00c15d0b4f
hunman89 <hunman89@gmail.com>
2022-03-11 21:40:40 +0900

    add. serving image

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -34,6 +34,10 @@ const EditProfile: NextPage = () => {
     if (user?.email) setValue("email", user?.email);
     if (user?.phone) setValue("phone", user.phone);
     if (user?.name) setValue("name", user.name);
+    if (user?.avatar)
+      setavatarPreview(
+        `https://imagedelivery.net/aSbksvJjax-AUC7qVnaC4A/${user?.avatar}/public`
+      );
   }, [user, setValue]);
   const [editProfile, { data, loading }] =
     useMutation<EditProfileResponse>(`/api/users/me`);
@@ -46,18 +50,22 @@ const EditProfile: NextPage = () => {
       });
     }
     if (avatar && avatar.length > 0 && user) {
-      const { id, uploadURL } = await (await fetch(`/api/files`)).json();
+      const { uploadURL } = await (await fetch(`/api/files`)).json();
       const form = new FormData();
       form.append("file", avatar[0], user?.id + "");
-      await fetch(uploadURL, {
-        method: "POST",
-        body: form,
-      });
+      const {
+        result: { id },
+      } = await (
+        await fetch(uploadURL, {
+          method: "POST",
+          body: form,
+        })
+      ).json();
       editProfile({
         email,
         phone,
         name,
-        //avatarUrl
+        avatarId: id,
       });
     } else {
       editProfile({ email, phone, name });
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -22,7 +22,14 @@ const Profile: NextPage = () => {
     <Layout title="My Page" hasTabBar>
       <div className="px-4">
         <div className="flex items-center space-x-3">
-          <div className="w-16 h-16 bg-slate-300 rounded-full" />
+          {user?.avatar ? (
+            <img
+              src={`https://imagedelivery.net/aSbksvJjax-AUC7qVnaC4A/${user?.avatar}/public`}
+              className="w-16 h-16 bg-slate-500 rounded-full"
+            />
+          ) : (
+            <div className="w-16 h-16 bg-slate-500 rounded-full" />
+          )}
           <div className="flex flex-col">
             <span className="font-medium text-gray-900">{user?.name}</span>
             <Link href="/profile/edit">
