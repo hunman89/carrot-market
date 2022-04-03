56ce26dc1dc74f0635ec7efcc85f6bdd8f333ff0
hunman89 <hunman89@gmail.com>
2022-04-03 21:37:36 +0900

    add. fonts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,25 +4,25 @@ import { disconnect } from "process";
 
 const client = new PrismaClient();
 
-async function main() {
-  [...Array.from(Array(100).keys())].forEach(async (item) => {
-    await client.stream.create({
-      data: {
-        name: String(item),
-        description: String(item),
-        price: item,
-        user: {
-          connect: {
-            id: 7,
-          },
-        },
-      },
-    });
-    setTimeout;
-    console.log(`${item}/100`);
-  });
-}
+// async function main() {
+//   [...Array.from(Array(100).keys())].forEach(async (item) => {
+//     await client.stream.create({
+//       data: {
+//         name: String(item),
+//         description: String(item),
+//         price: item,
+//         user: {
+//           connect: {
+//             id: 7,
+//           },
+//         },
+//       },
+//     });
+//     setTimeout;
+//     console.log(`${item}/100`);
+//   });
+// }
 
-main()
-  .catch((e) => console.log(e))
-  .finally(() => client.$disconnect());
+// main()
+//   .catch((e) => console.log(e))
+//   .finally(() => client.$disconnect());
